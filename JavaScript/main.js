df5f46226fc88cbe7b7b6dd38e1efe84d5ff94d2
PDX PRO <pdxpro0098@gmail.com>
2025-11-15 22:37:13 +0530

    add hollow Square to js and python

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -8,4 +8,20 @@ function solidSquare(line) {
     }
 }
 
-solidSquare(5);
+function hollowSquare(line) {
+    let pattern = "";
+    for (let i = 0; i < line; i++) {
+        for (let j = 0; j < line; j++) {
+            if ((i === 0 || i === line - 1) || (j === 0 || j === line - 1)) {
+                pattern += "*";
+            }
+            else {
+                pattern += " ";
+            }
+        }
+        pattern += "\n";
+    }
+    console.log(pattern);
+}
+
+hollowSquare(5);
--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,17 @@
 def solidSquare(line):
     for _ in range(line):
-        print("*"*line)
+        print("*" * line)
+
+
+def hollowSquare(line):
+    for i in range(line):
+        for j in range(line):
+            if ((i == 0 or i == line - 1) or (j == 0 or j == line - 1)):
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
 
 
 if __name__ == "__main__":
-    solidSquare(5)
+    hollowSquare(5)
