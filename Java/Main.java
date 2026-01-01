80d5f96fa5ac532965d5c7e8b6ea617f01c936a5
PDX PRO <pdxpro0098@gmail.com>
2026-01-01 22:33:49 +0530

    add hourglass pattern in java

--- Java/Main.java
+++ Java/Main.java
@@ -276,7 +276,40 @@ public class Main {
         }
     }
 
+    static void hourglassPattern(int line) {
+        for (int i = 0; i < line; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = line; j > i; j--) {
+                System.out.print("*");
+
+            }
+            for (int j = line - 1; j > i; j--) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < line; i++) {
+            for (int j = line; j > i + 1; j--) {
+                System.out.print(" ");
+
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+
+            }
+            for (int j = 1; j < i + 1; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        hollowDiamond(5);
+        hourglassPattern(5);
     }
 }
